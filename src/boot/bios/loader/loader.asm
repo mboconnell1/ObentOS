067a249a12fe7b317483e64eedcd9f4c6a9ca86c
Brandon <mboconnell1@proton.me>
2024-07-26 17:57:44 +0100

    Add A20 activation in bootloader

--- src/boot/bios/loader/loader.asm
+++ src/boot/bios/loader/loader.asm
@@ -3,7 +3,45 @@
 
         jmp     _start
 
+; Macros
+; ------------------------------------------------------------------------------
+%macro PRINT_STRING 1
+        push    bx
+        mov     bx, %1
+        call    __print_string
+        pop     bx
+%endmacro
+
+; Includes
+; ------------------------------------------------------------------------------
+%include "enable_a20.asm"
+%include "print_string.asm"
+
 ; Code
 ; ------------------------------------------------------------------------------
 _start:
+        cli
+        
+        xor     ax, ax
+        mov     ds, ax
+        mov     es, ax
+        mov     ss, ax
+        mov     sp, ax
+
+        sti
+
+        PRINT_STRING msg_enabling_a20
+        call    __enable_a20
+        jc      _halt
+        PRINT_STRING msg_success
+
         jmp     $
+_halt:
+        PRINT_STRING msg_hlt
+        jmp     $
+
+; Data
+; ------------------------------------------------------------------------------
+msg_enabling_a20:       db "Enabling A20 line... ", 0
+msg_success:            db "Success!", 13, 10, 0
+msg_hlt:                db 13, 10, "HALT", 0
